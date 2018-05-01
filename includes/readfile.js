9a61952df1fc68766e5aabe51b3bad12458460b2
Hallucinogenic Troll <luis.rei@ubi.pt>
2018-05-01 23:10:26 +0100

    dddd

--- includes/readfile.js
+++ includes/readfile.js
@@ -1,3 +1,14 @@
-setInterval(function() {
-    $('#element').load('file.txt');
-}, 1);
+    function WriteToFile(passForm) {
+     
+        set fso = CreateObject("Scripting.FileSystemObject"); 
+        set s   = fso.CreateTextFile("<your Path>/filename.txt", True);
+     
+        var firstName = document.getElementById('FirstName');
+        var lastName  = document.getElementById('lastName');
+     
+        s.writeline("First Name :" + FirstName);
+        s.writeline("Last Name :" + lastName);
+     
+        s.writeline("-----------------------------");
+        s.Close();
+     }
