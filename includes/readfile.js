132b1bfb0720ac8db30996e30c03733f84c54405
Hallucinogenic Troll <luis.rei@ubi.pt>
2018-05-01 22:56:18 +0100

    wda

--- includes/readfile.js
+++ includes/readfile.js
@@ -1,17 +1,3 @@
-document.getElementById('file').onchange = function(){
-
-  var file = this.files[0];
-
-  var reader = new FileReader();
-  reader.onload = function(progressEvent){
-    // Entire file
-    console.log(this.result);
-
-    // By lines
-    var lines = this.result.split('\n');
-    for(var line = 0; line < lines.length; line++){
-      console.log(lines[line]);
-    }
-  };
-  reader.readAsText(file);
-};
+setInterval(function() {
+    $('#element').load('file.txt');
+}, 1000);
